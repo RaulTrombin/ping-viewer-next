b4f1e1821dc61ebe6eb0093bb3b285d6a2cab5d1
Willian Galvani <williangalvani@gmail.com>
2025-02-12 16:50:08 -0300

    fix permission issues

--- blueos-ping-viewer-next/files/entrypoint.sh
+++ blueos-ping-viewer-next/files/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -m
 echo "Starting ping viewer next..."
-cd /app
-./ping-viewer-next --enable-auto-create --rest-server 0.0.0.0:6060
+cd app
+mkdir -p /app/logs
+chmod -R 755 /app/logs
+chown -R pingviewer:pingviewer /app/logs
+su pingviewer -c "./ping-viewer-next --enable-auto-create --rest-server 0.0.0.0:6060"
