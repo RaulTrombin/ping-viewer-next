afa855371063af7b430a6d5966164dab0fe39825
Raul Victor Trombin <raulvtrombin@gmail.com>
2025-12-08 18:03:24 -0300

    update required directory

--- blueos-ping-viewer-next/files/entrypoint.sh
+++ blueos-ping-viewer-next/files/entrypoint.sh
@@ -4,8 +4,11 @@ echo "Starting ping viewer next..."
 cd app
 mkdir logs
 mkdir recordings
+mkdir firmwares
 chmod -R 755 /app/logs
 chmod -R 755 /app/recordings
+chmod -R 755 /app/firmwares
 chown -R pingviewer:pingviewer /app/logs
 chown -R pingviewer:pingviewer /app/recordings
+chown -R pingviewer:pingviewer /app/firmwares
 su pingviewer -c "./ping-viewer-next --enable-auto-create --rest-server 0.0.0.0:6060"
